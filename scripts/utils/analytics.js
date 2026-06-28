175066478698c88d4607b165d1ac23047e6927f3
Tad Reeves <tadreeves@gmail.com>
2026-06-28 13:44:40 -0400

    Changed over analytics

--- scripts/utils/analytics.js
+++ scripts/utils/analytics.js
@@ -1,12 +1,12 @@
-const GA_ID = 'G-8HTDEY6V7P';
+const GA_IDS = ['G-8HTDEY6V7P', 'G-Q8BQNWZ0YL'];
 
 const script = document.createElement('script');
 script.async = true;
-script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_IDS[0]}`;
 document.head.appendChild(script);
 
 window.dataLayer = window.dataLayer || [];
 function gtag() { window.dataLayer.push(arguments); }
 window.gtag = gtag;
 gtag('js', new Date());
-gtag('config', GA_ID);
+GA_IDS.forEach((id) => gtag('config', id));

7d8844cbccd434688492859de7dccf10138ffe1e
Laurel Timko <laurel.timko@jmp.com>
2026-09-18 15:55:32 -0400

    Remove success

--- tools/preflight/preflight.js
+++ tools/preflight/preflight.js
@@ -194,8 +194,6 @@ const speakerCheck = async ({ doc }) => {
 
   if (!speakersMetadata) {
     return [REASONS['speakers.warn.missing']];
-  } else {
-    return { badge: 'success', reason: `Speaker(s) provided` };
   }
 };
 
